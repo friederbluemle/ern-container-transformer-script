da85727589fa410eca2c4d9ca03602156cab05fb
Benoit Lemaire <blemaire@walmartlabs.com>
2018-07-26 11:30:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Script Container Transformer
+
+This transformer allows for executing an arbitrary script to transform the Container in some custom way. It supports transformation of both iOS and Android Containers.
+
+## Inputs
+
+- `containerPath` : Path to the Container to transform
+- `scriptPath` : Path to the script to execute. It can be a local file path (for ex `/Users/username/scripts/myscript.sh`) or a path to a file stored in the Cauldron (for ex `cauldron://scripts/myscript.sh`). It is recommended to use the path to a file stored in the CauldronFor the `scriptPath` (the file needs to be added first to the cauldron through the [cauldron add file](https://native.electrode.io/cli-commands/cauldron-add/file) command).
+
+## Notes
+
+The script will be executed from the Container directory.
+
+## Usage
+
+### With `ern transform-container` CLI command
+
+```bash
+$ ern transform-container --containerPath [pathToContainer] -t script -e '{"scriptPath":"[pathToScript]"}'
+```
+
+Instead of passing the whole configuration on the command line for `--extra/-e`, it is also possible to use a file path to a json file holding the configuration, or a path to a file stored in the Cauldron. Check out the [ern transform-container](https://native.electrode.io/cli-commands/transform-container) command documentation for more info.
+
+### With Cauldron
+
+To automatically transform the Cauldron generated Containers of a target native application and platform, you can add a transformer entry in the Cauldron in the Container generator configuration object as follow :
+
+```json
+"transformers": [
+  {
+    "name": "script",
+    "extra": {
+      "scriptPath": "[pathToScript]"
+    }
+  }
+]
+```
+
+### Programmatically
+
+```js
+import ScriptTransformer from 'ern-container-transformer-script'
+const transformer = new ScriptTransformer()
+transformer.transform(
+  {
+    /* Local file system path to the Container */
+    containerPath: string
+    /* Extra data specific to this publisher */
+    extra?: {
+      scriptPath: string,
+    }
+  }
+})
+```
---
+++ src/ScriptTransformer.ts
@@ -0,0 +1,76 @@
+import fs from 'fs'
+import path from 'path'
+import shell from 'shelljs'
+import tmp from 'tmp'
+import { getActiveCauldron } from 'ern-cauldron-api'
+
+export interface ScriptTransformerExtra {
+  scriptPath: string
+}
+
+export default class ScriptTransformer {
+  /**
+   * Name of this transformer
+   */
+  get name(): string {
+    return 'script'
+  }
+
+  /**
+   * Supported platforms
+   */
+  get platforms(): string[] {
+    return ['ios', 'android']
+  }
+
+  /**
+   * Transform the container
+   */
+  public async transform({
+    containerPath,
+    extra,
+  }: {
+      containerPath: string
+      extra?: ScriptTransformerExtra
+    }) {
+    if (!extra) {
+      this.throwError('Missing extra property')
+    }
+
+    //
+    // Validate extra object (throw if invalid)
+    this.validate(extra!)
+
+    let pathToScript = extra!.scriptPath
+    if (pathToScript.startsWith('cauldron://')) {
+      const cauldron = await getActiveCauldron()
+      if (!cauldron) {
+        this.throwError('A Cauldron needs to be active for using a script stored in the Cauldron')
+      }
+      if (!await cauldron.hasFile({ cauldronFilePath: pathToScript })) {
+        this.throwError(`Cannot find ${pathToScript} in Cauldron`)
+      }
+      const scriptFile = await cauldron.getFile({ cauldronFilePath: pathToScript })
+      const scriptFileName = path.basename(pathToScript.replace('cauldron://', ''))
+      const tmpScriptDir = tmp.dirSync({ unsafeCleanup: true }).name
+      pathToScript = path.join(tmpScriptDir, scriptFileName)
+      fs.writeFileSync(pathToScript, scriptFile.toString())
+      shell.chmod('+x', pathToScript)
+    }
+
+    const scriptResult = shell.exec(pathToScript, {cwd:containerPath})
+    if (scriptResult.code !== 0) {
+      throw new Error(`Script execution failed with code ${scriptResult.code}`)
+    }
+  }
+
+  public validate(extra: ScriptTransformerExtra) {
+    if (!extra.scriptPath) {
+      this.throwError('Missing extra scriptPath')
+    }
+  }
+
+  public throwError(msg: string) {
+    throw new Error(`[ScriptTransformer] ${msg}`)
+  }
+}
